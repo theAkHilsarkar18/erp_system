bcfb91163888fd4e222cac9734a1cbb6e1a800c1
Your Name <your_email@example.com>
2025-01-29 15:51:39 +0530

    ....

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:pdf/pdf.dart';
+
 Color bgColor = const Color(0xffFFF0DC);
 Color secondary = const Color(0xffF0BB78);
 Color black = const Color(0xff131010);
--- lib/pdf/generate_pdf.dart
+++ lib/pdf/generate_pdf.dart
@@ -2,6 +2,7 @@
 // process of creating pdf.
 import 'dart:typed_data';
 
+import 'package:erp_system/colors.dart';
 import 'package:erp_system/utils.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
@@ -10,6 +11,10 @@ import 'package:pdf/widgets.dart' as pw;
 // pdf/widget --> pdf -> pw.Container()
 
 Future<Uint8List> generatePdf(Map student) async {
+
+
+  final image = pw.MemoryImage(student['image']);
+
   final pdf = pw.Document(); // create document - blank
   pdf.addPage(
     pw.Page(
@@ -19,13 +24,28 @@ Future<Uint8List> generatePdf(Map student) async {
         return pw.Column(
           crossAxisAlignment: pw.CrossAxisAlignment.start,
           children: [
-            pw.Text(student['id'],style: const pw.TextStyle(color: PdfColors.blue,fontSize: 30)),
-            pw.Text(student['name'],style: const pw.TextStyle(color: PdfColors.blue,fontSize: 30)),
-            pw.Text(student['age'],style: const pw.TextStyle(color: PdfColors.blue,fontSize: 30)),
-            pw.Text(student['phone'],style: const pw.TextStyle(color: PdfColors.blue,fontSize: 30)),
-            pw.Text(student['course'],style: const pw.TextStyle(color: PdfColors.blue,fontSize: 30)),
-            pw.Text(student['address'],style: const pw.TextStyle(color: PdfColors.blue,fontSize: 30)),
-          ],
+            pw.Container(
+              height: 200,
+              width: 200,
+              decoration: pw.BoxDecoration(
+                shape: pw.BoxShape.circle,
+                image: pw.DecorationImage(image: pw.MemoryImage(student['image'])),
+              )
+            ),
+            pw.SizedBox(height: 10),
+            pw.Divider(),
+            pw.SizedBox(height: 10),
+
+            pw.Text(student['id'],style:  pw.TextStyle(color: PdfColors.amber800,fontSize: 30,fontWeight: pw.FontWeight.bold)),
+            pw.SizedBox(height: 10),
+            pw.Text(student['name'],style:  pw.TextStyle(color: PdfColors.amber800,fontSize: 25,fontWeight: pw.FontWeight.bold)),
+            pw.Text("Age: "+student['age'],style:  pw.TextStyle(color: PdfColors.amber800,fontSize: 25,fontWeight: pw.FontWeight.bold)),
+            pw.Text(student['course'],style:  pw.TextStyle(color: PdfColors.amber800,fontSize: 25,fontWeight: pw.FontWeight.bold)),
+            pw.Text(student['phone'],style:  pw.TextStyle(color: PdfColors.amber800,fontSize: 25,fontWeight: pw.FontWeight.bold)),
+            pw.SizedBox(height: 10),
+
+            pw.Text(student['address'],style:  const pw.TextStyle(color: PdfColors.amber800,fontSize: 20)),
+              ],
         );
       },
     ),
